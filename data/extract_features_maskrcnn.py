ba16e8cd214a0a1ff0d6d62369dd542bfb1db77f
Yash Kant <ysh.kant@gmail.com>
2019-07-04 15:55:21 +0530

    Add extract utils

--- data/extract_features_maskrcnn.py
+++ data/extract_features_maskrcnn.py
@@ -1,6 +1,4 @@
 import argparse
-import glob
-import os
 import torch
 import cv2  # must import before importing caffe2 due to bug in cv2
 from torch.utils.data import DataLoader
@@ -9,9 +7,9 @@ import h5py
 import yaml
 import sys
 import numpy as np
+import os
 
 sys.path.append("..")  # add parent to import modules
-import os
 # path to packages inside captioning are already available to interpreter
 from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.modeling.detector import (
@@ -22,12 +20,11 @@ from maskrcnn_benchmark.utils.model_serialization import (
 )
 from captioning.utils import process_feature_extraction, pad_raw_image_batch
 from visdialch.data.dataset import RawImageDataset
+from extract_utils import collate_function, rearrange_ouput, get_range_path
 
 # OpenCL may be enabled by default in OpenCV3; disable it because it's not
 # thread safe and causes unwanted GPU memory allocations.
 cv2.ocl.setUseOpenCL(False)
-
-# TODO: See if torchvision.datasets.ImageFolder can help simplify data loading
 
 parser = argparse.ArgumentParser(
     description="Extract bottom-up features from a model trained by Detectron"
@@ -106,23 +103,6 @@ torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 
 
-def image_id_from_path(image_path):
-    """Given a path to an image, return its id.
-
-    Parameters
-    ----------
-    image_path : str
-        Path to image, e.g.: coco_train2014/COCO_train2014/000000123456.jpg
-
-    Returns
-    -------
-    int
-        Corresponding image id (123456)
-    """
-
-    return int(image_path.split("/")[-1][-16:-4])
-
-
 def main(args):
     """Extract bottom-up features from all images in a directory using
     a pre-trained Detectron model, and save them in HDF format.
@@ -142,6 +122,14 @@ def main(args):
         visdial_path + caption_config["detectron_model"]["config_yaml"]
     )
     cfg.freeze()
+
+    # use this to extract features from different subsets of the datasets
+    # by spawning multiple processes and finally merging the files,
+    # this is recommended instead of using dataparallel model
+    restrict_range = None
+    if args.start_range is not None and args.stop_range is not None:
+        restrict_range = [args.start_range, args.stop_range]
+        args.save_path = get_range_path(args.save_path, restrict_range)
 
     if isinstance(args.gpu_ids, int):
         args.gpu_ids = [args.gpu_ids]
@@ -168,34 +156,15 @@ def main(args):
     load_state_dict(detection_model, checkpoint.pop("model"))
     detection_model.eval()
 
-    # used to handle variable size images inside the dataloader
-    def collate_function(batch):
-        # print(f"Debug: collate_function, target shape {len(batch)}, {type(batch[0])}")
-        # I think batch is (n, item)
-        item_batch = {}
-        item_batch["image"] = []
-        item_batch["im_scales"] = []
-        item_batch["image_ids"] = []
-
-        for item in batch:
-            item_batch["image"].append(item["image"])
-            item_batch["im_scales"].append(item["im_scale"])
-            item_batch["image_ids"].append(item["image_id"])
-
-        item_batch["image"], item_batch["image_size"] = pad_raw_image_batch(
-            item_batch["image"],
-            32
-        )
-        return item_batch
-    
     print("Model Loaded, Loading Dataset now...")
-    
-    restrict_range = None
-    if args.start_range is not None and args.stop_range is not None:
-        args.save_path = args.save_path.split('.')[0] + "_range_" + str(args.start_range) + "_to_" + str(args.stop_range) + '.h5'
-        restrict_range = [args.start_range, args.stop_range]
 
-    raw_image_dataset = RawImageDataset(args.image_root, args.split, True, False, restrict_range)
+    raw_image_dataset = RawImageDataset(
+        args.image_root,
+        args.split,
+        trasform=True,
+        in_mem=False,
+        restrict_range=restrict_range
+    )
     raw_image_dataloader = DataLoader(
         raw_image_dataset,
         batch_size=args.batch_size,
@@ -203,6 +172,7 @@ def main(args):
         shuffle=False,
         collate_fn=collate_function
     )
+    print("Dataset Loaded")
 
     # create an output HDF to save extracted features
     save_h5 = h5py.File(args.save_path, "w")
@@ -222,15 +192,6 @@ def main(args):
     scores_h5d = save_h5.create_dataset(
         "scores", (len(raw_image_dataset), args.max_boxes,),
     )
-    print("Dataset Loaded")
-
-    # rearrange output from the model from process_feat_extraction function
-    def rearrange_ouput(output):
-        for key in output[0].keys():
-            if isinstance(output[0][key], torch.Tensor):
-                embed_dim = output[0][key].shape[-1]
-                output[0][key] = output[0][key].view(-1, embed_dim)
-        return output
 
     for i, batch in enumerate(tqdm(raw_image_dataloader)):
 
@@ -253,7 +214,6 @@ def main(args):
         feat_name = caption_config["detectron_model"]["feat_name"]
         get_boxes = True
 
-        # use NMS and preserve only the ``args.max_boxes`` features
         boxes, features, classes, scores = process_feature_extraction(
             output,
             batch["im_scales"],
